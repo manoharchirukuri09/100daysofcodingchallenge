b306e4e225e68c72c6239f6ee276adc55fc56a0f
Manoharchirukuri <manoharchirukuri09@gmail.com>
2025-07-14 22:38:55 +0530

    Create Day44.java

---
+++ Day44.java
@@ -0,0 +1,55 @@
+2. Add Two Numbers
+  class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode dummyHead = new ListNode(0);
+        ListNode tail = dummyHead;
+        int carry = 0;
+
+        while (l1 != null || l2 != null || carry != 0) {
+            int digit1 = (l1 != null) ? l1.val : 0;
+            int digit2 = (l2 != null) ? l2.val : 0;
+
+            int sum = digit1 + digit2 + carry;
+            int digit = sum % 10;
+            carry = sum / 10;
+
+            ListNode newNode = new ListNode(digit);
+            tail.next = newNode;
+            tail = tail.next;
+
+            l1 = (l1 != null) ? l1.next : null;
+            l2 = (l2 != null) ? l2.next : null;
+        }
+
+        ListNode result = dummyHead.next;
+        dummyHead.next = null;
+        return result;
+    }
+}
+
+
+92. Reverse Linked List II
+  class Solution {
+    public ListNode reverseBetween(ListNode head, int left, int right) {
+       if (head == null || left == right) return head;
+
+        ListNode dummy = new ListNode(-1);
+        dummy.next = head;
+        ListNode prev = dummy;
+
+        for (int i = 0; i < left - 1; i++) {
+            prev = prev.next;
+        }
+
+        ListNode curr = prev.next;
+
+        for (int i = 0; i < right - left; i++) {
+            ListNode temp = curr.next;
+            curr.next = temp.next;
+            temp.next = prev.next;
+            prev.next = temp;
+        }
+
+        return dummy.next; 
+    }
+}
