b0a08465999196035b4450a632d91f478de5e7fc
Manoharchirukuri <manoharchirukuri09@gmail.com>
2025-07-19 19:17:50 +0530

    Create Day49.java

---
+++ Day49.java
@@ -0,0 +1,54 @@
+24. Swap Nodes in Pairs
+  class Solution {
+    public ListNode swapPairs(ListNode head) {
+        if(head==null || head.next==null) return head;
+        
+        ListNode ptr = head;
+        while(ptr!=null && ptr.next!=null){
+            int temp = ptr.val;
+            ptr.val = ptr.next.val;
+            ptr.next.val = temp;
+            ptr = ptr.next.next;
+        }
+
+        return head;
+    }
+}
+
+143. Reorder List
+   class Solution {
+    public void reorderList(ListNode head) {
+        if (head == null) return;
+
+        // Step 1: Find the middle of the list
+        ListNode slow = head, fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        // Step 2: Reverse the second half of the list
+        ListNode second = slow.next;
+        slow.next = null;
+        ListNode node = null;
+
+        while (second != null) {
+            ListNode temp = second.next;
+            second.next = node;
+            node = second;
+            second = temp;
+        }
+
+        // Step 3: Merge the two halves
+        ListNode first = head;
+        second = node;
+
+        while (second != null) {
+            ListNode temp1 = first.next, temp2 = second.next;
+            first.next = second;
+            second.next = temp1;
+            first = temp1;
+            second = temp2;
+        }        
+    }
+}
