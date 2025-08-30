8490a3b8db1b79ed1982ed964ddfad120d7fefd2
Manoharchirukuri <manoharchirukuri09@gmail.com>
2025-08-30 10:56:32 +0530

    Create Day91.java

---
+++ Day91.java
@@ -0,0 +1,59 @@
+1624. Largest Substring Between Two Equal Characters
+   class Solution {
+    public int maxLengthBetweenEqualCharacters(String s) {
+        Map<Character, Integer> lastOccurrence = new HashMap<>();
+        int maxLength = -1;
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (lastOccurrence.containsKey(c)) {
+                maxLength = Math.max(maxLength, i - lastOccurrence.get(c) - 1);
+            } else {
+                lastOccurrence.put(c, i);
+            }
+        }
+
+        return maxLength;
+    }
+}
+
+567. Permutation in String
+   class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+        if (s1.length() > s2.length()) {
+            return false;
+        }
+        
+        HashMap<Character, Integer> s1Count = new HashMap<>();
+        HashMap<Character, Integer> s2Count = new HashMap<>();
+        
+        for (int i = 0; i < s1.length(); i++) {
+            s1Count.put(s1.charAt(i), s1Count.getOrDefault(s1.charAt(i), 0) + 1);
+            s2Count.put(s2.charAt(i), s2Count.getOrDefault(s2.charAt(i), 0) + 1);
+        }
+        
+        if (s1Count.equals(s2Count)) {
+            return true;
+        }
+        
+        int left = 0;
+        for (int right = s1.length(); right < s2.length(); right++) {
+            char charRight = s2.charAt(right);
+            s2Count.put(charRight, s2Count.getOrDefault(charRight, 0) + 1);
+            
+            char charLeft = s2.charAt(left);
+            s2Count.put(charLeft, s2Count.get(charLeft) - 1);
+            if (s2Count.get(charLeft) == 0) {
+                s2Count.remove(charLeft);
+            }
+            
+            left++;
+            
+            if (s1Count.equals(s2Count)) {
+                return true;
+            }
+        }
+        
+        return false;        
+    }
+}
