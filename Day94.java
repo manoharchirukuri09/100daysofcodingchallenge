b53748495c96e2186cfbf5c444503d8ba970e11a
Manoharchirukuri <manoharchirukuri09@gmail.com>
2025-09-02 10:36:14 +0530

    Create Day94.java

---
+++ Day94.java
@@ -0,0 +1,48 @@
+628. Maximum Product of Three Numbers
+    class Solution {
+    public int maximumProduct(int[] nums) {
+        int max1 = Integer.MIN_VALUE;
+        int max2 = Integer.MIN_VALUE;
+        int max3 = Integer.MIN_VALUE;
+        int min1 = Integer.MAX_VALUE;
+        int min2 = Integer.MAX_VALUE;
+        for (int n: nums) {
+            if (n > max1) {
+                max3 = max2;
+                max2 = max1;
+                max1 = n;
+            } else if (n > max2) {
+                max3 = max2;
+                max2 = n;
+            } else if (n > max3) {
+                max3 = n;
+            }
+
+            if (n < min1) {
+                min2 = min1;
+                min1 = n;
+            } else if (n < min2) {
+                min2 = n;
+            }
+        }
+        return Math.max(max1 * max2 * max3, min1 * min2 * max1);
+    }
+}
+
+453. Minimum Moves to Equal Array Elements
+  class Solution {
+    public int minMoves(int[] nums) {
+        int n = nums.length;
+    int min = Integer.MAX_VALUE;
+    int ans = 0;
+    
+    for(int i =0; i<n; i++)
+        min = Math.min(min,nums[i]);
+    
+    for(int i = 0; i<n; i++){
+        ans += nums[i] - min;
+    }
+    
+    return ans;
+}
+    }
